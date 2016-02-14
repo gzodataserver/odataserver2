d4fb75e4e8acf42c367b8bea4f5702a5287ad306
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2016-02-14 19:20:12 +0100

    Added assertions in mysql test

--- test_mysql.js
+++ test_mysql.js
@@ -1,3 +1,4 @@
+var assert = require('assert');
 var querystring = require("querystring");
 var remote = require('gzhelpers').remote;
 var jsonParseArray = require('jsonparsearray');
@@ -40,7 +41,18 @@ var createOptions = function (accountId, password, path, method) {
 // json parser
 var p = new jsonParseArray();
 
+var assertJSON = function (jsonArray, res, expected, message) {
+  jsonArray.clear();
+  jsonArray.write(res);
+  try {
+    assert.deepEqual(jsonArray.get(), expected, message);
+  } catch (err) {
+    error(err);
+  }
+}
+
 log('A web server should be running on localhost:3000');
+log('\n!!No assertion errors means everything is ok!"\n')
 
 remote.request(createOptions(ACCOUNTID, PASSWORD, '/create_account', 'POST'), {
     email: EMAIL
@@ -77,7 +89,19 @@ remote.request(createOptions(ACCOUNTID, PASSWORD, '/create_account', 'POST'), {
     });
   })
   .then(function (res) {
-    log(res);
+    assertJSON(p, res, [{
+        queryType: 'create_table'
+      },
+      {
+        fieldCount: 0,
+        affectedRows: 0,
+        insertId: 0,
+        serverStatus: 2,
+        warningCount: 0,
+        message: '',
+        protocol41: true,
+        changedRows: 0
+      }], 'create_table');
 
     // INSERT INTO
     var path = '/' + ACCOUNTID + '/mytable';
@@ -87,14 +111,29 @@ remote.request(createOptions(ACCOUNTID, PASSWORD, '/create_account', 'POST'), {
     });
   })
   .then(function (res) {
-    log(res);
+    assertJSON(p, res, [{
+      "fieldCount": 0,
+      "affectedRows": 1,
+      "insertId": 0,
+      "serverStatus": 2,
+      "warningCount": 0,
+      "message": "",
+      "protocol41": true,
+      "changedRows": 0
+    }], 'POST mytable')
+
 
     // SELECT
     var path = '/' + ACCOUNTID + '/mytable';
     return remote.request(createOptions(ACCOUNTID, PASSWORD, path, 'GET'), null);
   })
   .then(function (res) {
-    log(res);
+    assertJSON(p, res, [{
+      "queryType": "select"
+    }, {
+      "col1": 22,
+      "col2": "22"
+    }], 'GET mytable')
 
     // UPDATE
     var path = '/' + ACCOUNTID + '/mytable';
@@ -103,7 +142,16 @@ remote.request(createOptions(ACCOUNTID, PASSWORD, '/create_account', 'POST'), {
     });
   })
   .then(function (res) {
-    log(res);
+    assertJSON(p, res, [{
+      "fieldCount": 0,
+      "affectedRows": 1,
+      "insertId": 0,
+      "serverStatus": 34,
+      "warningCount": 0,
+      "message": "(Rows matched: 1  Changed: 1  Warnings: 0",
+      "protocol41": true,
+      "changedRows": 1
+    }], 'PUT mytable');
 
     // GRANT
     var path = '/' + ACCOUNTID + SYS_PATH + '/grant';
@@ -113,7 +161,18 @@ remote.request(createOptions(ACCOUNTID, PASSWORD, '/create_account', 'POST'), {
     });
   })
   .then(function (res) {
-    log(res);
+    assertJSON(p, res, [{
+      "queryType": "grant"
+    }, {
+      "fieldCount": 0,
+      "affectedRows": 0,
+      "insertId": 0,
+      "serverStatus": 2,
+      "warningCount": 0,
+      "message": "",
+      "protocol41": true,
+      "changedRows": 0
+    }], 'grant');
 
     // REVOKE
     var path = '/' + ACCOUNTID + SYS_PATH + '/revoke';
@@ -123,24 +182,48 @@ remote.request(createOptions(ACCOUNTID, PASSWORD, '/create_account', 'POST'), {
     });
   })
   .then(function (res) {
-    log(res);
+    assertJSON(p, res, [{
+      "queryType": "revoke"
+    }, {
+      "fieldCount": 0,
+      "affectedRows": 0,
+      "insertId": 0,
+      "serverStatus": 2,
+      "warningCount": 0,
+      "message": "",
+      "protocol41": true,
+      "changedRows": 0
+    }], 'revoke');
 
     // METADATA
     var path = '/' + ACCOUNTID + '/mytable/$metadata';
     return remote.request(createOptions(ACCOUNTID, PASSWORD, path, 'GET'), null);
   })
   .then(function (res) {
-    log(res);
+    assertJSON(p, res, [{
+      "queryType": "metadata"
+    }, {
+      "column_name": "col1",
+      "data_type": "int",
+      "is_nullable": "YES",
+      "numeric_precision": 10,
+      "numeric_scale": 0
+    }, {
+      "column_name": "col2",
+      "data_type": "varchar",
+      "is_nullable": "YES",
+      "numeric_precision": null,
+      "numeric_scale": null
+    }], 'metdata');
 
     // INCORRECT BUCKET ADMIN OP
-    log('Testing incorrect bucket admin op.')
     var path = '/' + ACCOUNTID + SYS_PATH + '/create_bucket2';
     return remote.request(createOptions(ACCOUNTID, PASSWORD, path, 'POST'), {
       name: 'b_mybucket'
     });
   })
   .catch(function (res) {
-    log(res);
+    assert(res, 'problem with request: Parse Error', 'incorrect bucket admin op');
 
     // FILTER & ORDER BY
     var params = querystring.stringify({
@@ -154,7 +237,9 @@ remote.request(createOptions(ACCOUNTID, PASSWORD, '/create_account', 'POST'), {
     return remote.request(createOptions(ACCOUNTID, PASSWORD, path, 'GET'), null);
   })
   .then(function (res) {
-    log(res);
+    assertJSON(p, res, [{
+      "queryType": "select"
+    }], 'GET mytable with filter');
 
     // FILTER, COLS, ORDER BY
     var params = querystring.stringify({
@@ -166,7 +251,12 @@ remote.request(createOptions(ACCOUNTID, PASSWORD, '/create_account', 'POST'), {
     return remote.request(createOptions(ACCOUNTID, PASSWORD, path, 'GET'), null);
   })
   .then(function (res) {
-    log(res);
+    assertJSON(p, res, [{
+      "queryType": "select"
+    }, {
+      "col1": 22,
+      "col2": "33"
+    }], 'GET mytable with filter');
 
     // ORDER BY
     var params = querystring.stringify({
@@ -176,7 +266,12 @@ remote.request(createOptions(ACCOUNTID, PASSWORD, '/create_account', 'POST'), {
     return remote.request(createOptions(ACCOUNTID, PASSWORD, path, 'GET'), null);
   })
   .then(function (res) {
-    log(res);
+    assertJSON(p, res, [{
+      "queryType": "select"
+    }, {
+      "col1": 22,
+      "col2": "33"
+    }], 'GET mytable with filter');
 
     // DELETE
     var filter = querystring.stringify({
@@ -188,7 +283,18 @@ remote.request(createOptions(ACCOUNTID, PASSWORD, '/create_account', 'POST'), {
       accountId: ACCOUNTID2
     });
   }).then(function (res) {
-    log(res);
+    assertJSON(p, res, [{
+      "queryType": "delete"
+    }, {
+      "fieldCount": 0,
+      "affectedRows": 1,
+      "insertId": 0,
+      "serverStatus": 34,
+      "warningCount": 0,
+      "message": "",
+      "protocol41": true,
+      "changedRows": 0
+    }], 'DELTE mytable');
 
     // DROP TABLE
     var path = '/' + ACCOUNTID + SYS_PATH + '/delete_table';
@@ -197,14 +303,25 @@ remote.request(createOptions(ACCOUNTID, PASSWORD, '/create_account', 'POST'), {
     });
   })
   .then(function (res) {
-    log(res);
+    assertJSON(p, res, [{
+      "queryType": "delete_table"
+    }, {
+      "fieldCount": 0,
+      "affectedRows": 0,
+      "insertId": 0,
+      "serverStatus": 2,
+      "warningCount": 0,
+      "message": "",
+      "protocol41": true,
+      "changedRows": 0
+    }], 'POST delete_table');
 
     // SERVICE DEF
     var path = '/' + ACCOUNTID;
     return remote.request(createOptions(ACCOUNTID, PASSWORD, path, 'GET'), null);
   })
   .then(function (res) {
-    log(res);
+    assertJSON(p, res, [{"queryType":"service_def"}], 'GET / (service def)');
 
     // DELETE ACCOUNT
     var path = '/' + ACCOUNTID + SYS_PATH + '/delete_account';
