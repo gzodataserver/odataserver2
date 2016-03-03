3b3942c56158870d800e69d26c0c87d6cd031f86
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2016-03-03 15:35:25 +0100

    Added bat file for setenv

---
+++ setenv.template.bat
@@ -0,0 +1,9 @@
+set ADMIN_USER=root
+set ADMIN_PASSWORD=secret
+set MAIL_USER=mail@example.com
+set MAIL_PASSWORD=secret
+
+# This controls the debugging
+#set NODE_DEBUG=debugtest,debugmysql,debugleveldb,debugodata,debugmain
+set NODE_DEBUG=debugodata,debugmysql,debugmain
+#
