428855fd2765bb39b0dfb1202d330858a674c0a4
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2016-04-21 14:29:21 +0200

    fix error in test

--- test_mysql.js
+++ test_mysql.js
@@ -27,7 +27,7 @@ var SYS_PATH = '/s';
 
 var createOptions = function (accountId, password, path, method) {
   return {
-    hostname: process.env.DB_HOST,
+    hostname: 'localhost',
     port: 3000,
     path: path,
     method: method,
